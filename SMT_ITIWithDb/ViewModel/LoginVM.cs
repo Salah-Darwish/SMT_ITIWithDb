025dabf7c961bb47c00d1e7e0fea3fd02983b7b6
Salah Darwish <100836109+Salah-Darwish@users.noreply.github.com>
2023-09-30 02:26:59 +0300

    insert AccountController and Login and register

---
+++ SMT_ITIWithDb/ViewModel/LoginVM.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Build.Framework;
+
+namespace SMT_ITIWithDb.ViewModels
+{
+    public class LoginVM
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Location { get; set; }
+     
+        public bool IsOffice { get; set; }
+    }
+}
